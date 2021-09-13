895b71d6a8021967781278c70cd092108925c8cf
brianorora <brianorora@gmail.com>
2021-09-13 18:19:47 +0300

    Add Books container styling

--- script.js
+++ script.js
@@ -3,31 +3,21 @@ class BookLibrary {
     this.booksCollection = document.querySelector('.books');
     this.bookList = JSON.parse(localStorage.getItem('books')) || [];
     this.bookList.forEach((book) => {
-      const div = document.createElement('div');
-      div.innerHTML = `
-        <h2>${book.title}</h2>
-        <p>${book.author}</p>
-      `;
-
-      const removeBtn = document.createElement('button');
-      removeBtn.innerText = 'Remove';
-      removeBtn.addEventListener('click', () => {
-        this.removeBook(div, book.title, book.author);
-      });
-
-      div.appendChild(removeBtn);
-
-      this.booksCollection.appendChild(div);
+      this.createBookElement(book.title, book.author);
     });
   }
 
   addBook(title, author) {
     this.bookList.push({ title, author });
     localStorage.setItem('books', JSON.stringify(this.bookList));
+    this.createBookElement(title, author);
+  }
+
+  createBookElement(title, author) {
     const div = document.createElement('div');
+    div.classList.add('book');
     div.innerHTML = `
-    <h2>${title}</h2>
-    <p>${author}</p>
+    <p>"${title}" by ${author}</p>
     `;
 
     const removeBtn = document.createElement('button');
