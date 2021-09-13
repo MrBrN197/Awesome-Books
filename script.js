b18131b26ad8e8237ba9dc00905573495c53285c
brianorora <brianorora@gmail.com>
2021-09-13 13:33:00 +0300

    Fix: linter errors

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const addBtn = document.querySelector('.add-book-btn');
 let bookList = [];
+
+function addBook(title, author) {
+  bookList.push({ title, author });
+  localStorage.setItem('books', JSON.stringify(bookList));
+}
+
+function removeBook(element, title, author) {
+  element.remove();
+  bookList = bookList.filter((book) => book.title !== title || book.author !== author);
+  localStorage.setItem('books', JSON.stringify(bookList));
+}
+
+const addBtn = document.querySelector('.add-book-btn');
+
 addBtn.addEventListener('click', () => {
   // add book
 
@@ -17,26 +30,10 @@ addBtn.addEventListener('click', () => {
   const removeBtn = document.createElement('button');
   removeBtn.innerText = 'Remove';
   removeBtn.addEventListener('click', () => {
-    console.log(title, author)
-   removeBook(div, title, author)
+    removeBook(div, title, author);
   });
 
   div.appendChild(removeBtn);
 
   booksCollection.appendChild(div);
 });
-
-function addBook(title, author) {
-  bookList.push({title, author});
-  localStorage.setItem('books', JSON.stringify(bookList))
-}
-
-function removeBook(element, title, author) {
-  element.remove();
-  console.log(title, author);
-  bookList = bookList.filter( (book) => {
-    return book.title !== title || book.author !== author
-  })
-  localStorage.setItem('books', JSON.stringify(bookList))
-  console.log(bookList)
-}
